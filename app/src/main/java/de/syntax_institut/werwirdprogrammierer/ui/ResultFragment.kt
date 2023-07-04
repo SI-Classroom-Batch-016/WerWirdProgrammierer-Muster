d70ed8d17f4ce96a968343685e6efb96e5ea9c55
FabianAMoreno <fabian.moreno@syntax-institut.de>
2023-07-04 17:04:35 +0200

    dataBinding -> viewBinding

--- app/src/main/java/de/syntax_institut/werwirdprogrammierer/ui/ResultFragment.kt
+++ app/src/main/java/de/syntax_institut/werwirdprogrammierer/ui/ResultFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -24,7 +23,7 @@ class ResultFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
+        binding = FragmentResultBinding.inflate(inflater, container, false)
         return binding.root
     }
 
